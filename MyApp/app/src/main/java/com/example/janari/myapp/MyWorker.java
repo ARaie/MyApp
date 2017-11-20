da8e1522df8bb1689757fcefef4e48cc3c9e1752
ARaie <annika_875@hotmail.com>
2017-11-20 22:14:07 +0200

    firstView

---
+++ MyApp/app/src/main/java/com/example/janari/myapp/MyWorker.java
@@ -0,0 +1,13 @@
+package com.example.janari.myapp;
+
+/**
+ * Created by Janari on 11/20/2017.
+ */
+
+public class MyWorker {
+
+    public static double calculateSum (double value, double sum){
+
+        return value - sum;
+    }
+}
